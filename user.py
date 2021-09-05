fdf1ba0768cb18ece343b5dc1f1d90753c9ff898
mash <mashalonzo741@gmail.com>
2021-09-05 12:42:54 +0300

    Initialize the user class properties

--- user.py
+++ user.py
@@ -2,3 +2,22 @@ class User:
     """
     Class that generates new instance of the users
     """
+
+    user_list = [] #empty user list
+
+    def __init__(self,first_name,last_name,user_name,password):
+
+        """
+        __init__method that helps to define properties of our objects
+        
+        Args:
+            first_name: New user first name
+            last_name: New user last name
+            user_name: New user login name
+            password: New user password
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.password = password
