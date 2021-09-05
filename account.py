99111869dc1c5a8a4263754b6083f30038edc4a2
mash <mashalonzo741@gmail.com>
2021-09-05 13:36:28 +0300

    Initialize the account properties and its tests

--- account.py
+++ account.py
@@ -2,3 +2,17 @@ class Account:
     """
     Class that generates new instances of accounts
     """
+
+    account_list = []
+    
+    def __init__(self,account_name,password):
+        """
+        __init__ method that helps us define the properties for our objects
+        
+        Args:
+            account_name: New account name
+            password: New account password
+        """
+
+        self.acc_name = account_name
+        self.password = password
---
+++ account_test.py
@@ -0,0 +1,28 @@
+import unittest
+from account import Account
+
+class TestAccount(unittest.TestCase):
+
+    """
+    Test class that defines test cases for the contact class behaviours
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    """
+
+
+    def setUp(self):
+        """
+        Set up method to run before each test cases
+        """
+
+        self.new_account = Account("Twitter","twinklestar")
+
+
+    def test_init(self):
+        """
+        test_init test case to test if the object is initialized properly
+        """
+
+        self.assertEqual(self.new_account.acc_name,"Twitter")
+        self.assertEqual(self.new_account.password,"twinklestar")
